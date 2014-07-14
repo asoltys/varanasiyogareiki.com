8509f1bf39723668f403b96ca60b7f88ed5b1fe5
Adam Soltys <asoltys@gmail.com>
2014-07-14 15:55:12 -0700

    Basic front page

--- index.php
+++ index.php
@@ -1,1 +1,71 @@
-<h1>Coming Soon!</h1>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>Varanasi Yoga Reiki</title>
+    <link href="img/favicon.ico?v=2" rel="shortcut icon" />
+    <link rel='stylesheet' href='/css/main.css'>
+    <script src="/js/jquery-1.8.2.min.js"></script>
+    <script src="/js/application.js"></script>
+
+    <meta name="description" content="Yoga and Reiki studio in Varanasi, India" />
+    <meta name="keywords" content="Yoga, Reiki, Varanasi" />
+
+  </head>
+  <body>
+    <div id="wrap">
+      <div id="main" class="container clear-top">
+        <div class="navbar navbar-inverse navbar-static-top">
+          <div class="navbar-inner">
+            <a class="brand" href="/">Varanasi Yoga Reiki</a>
+          </div>
+        </div>
+        <div class="hero-unit">
+          <p>Come visit our studio in the Varanasi old town to take classes with Yogi Ajay Menon, a certified Yoga and Reiki pracitioner who has been teaching for more than 30 years.</p>
+        </div>
+
+        <div class="row">
+
+          <div class="span6">
+            <h2><i class="icon-user"></i> About us</h2>
+
+            <p>We offer both individual and group Yoga classes and Reiki sessions by appointment or during the following scheduled times.</p>
+
+            <div class="row">
+              <div class="span3">
+                <h4>Yoga</h4>
+                <p>Monday, 11:00am-12:00pm<br />
+                Wednesday, 11:00am-12:00pm<br />
+                Friday, 11:00am-12:00pm</p>
+              </div>
+
+              <div class="span3">
+                <h4>Reiki</h4>
+                <p>Monday, 11:00am-12:00pm<br />
+                Wednesday, 11:00am-12:00pm<br />
+                Friday, 11:00am-12:00pm</p>
+              </div>
+            </div>
+
+            <img src="/img/varanasi.jpg" />
+          </div>
+
+          <div class="span6">
+            <h2><i class="icon-user"></i> Contact</h2>
+            <p>(Address)<br />(Address)<br />(Address)</p>
+            <p>Please call +91 09651904397 or email <a href="yogi_menon_varanasi@yahoo.com">yogi_menon_varanasi@yahoo.com</a> to book an appointment!</p>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1803.428353008759!2d83.01244!3d25.309017999999988!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x398e2e1fcbd0a33d%3A0x6d352e75dd34885!2sMeer+Ghat!5e0!3m2!1sen!2s!4v1405377527538" width="800" height="450" frameborder="0" style="border:0"></iframe>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+    <footer class="footer navbar navbar-inverse brand">
+      <div class="pull-left">
+        <a class="brand" href="http://facebook.com/varanasiyogareiki"><i class="icon-facebook icon-large"></i></a>
+        <a class="brand" href="mailto:yogi_menon_varanasi@yahoo.com"><i class="icon-envelope"></i></a>
+      </div>
+    </footer>
+    </div>
+  </body>
+<
